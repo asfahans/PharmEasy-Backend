bc975a0a82eae6c8278367694f5b1f2564817507
Asfahan Shaikh <asfahans@gmail.com>
2021-06-08 15:31:10 +0530

    edited Product Schema

--- models/product.js
+++ models/product.js
@@ -33,7 +33,7 @@ const productSchema = mongoose.Schema({
   category: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Category',
-    required: true,
+    required: false, // Originally it was true
   },
   countInStock: {
     type: Number,
